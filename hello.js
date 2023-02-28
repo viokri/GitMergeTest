0f313d5da6ae8acef82f7c05ee34262a7e4a2cc9
monalloyd <monapluslloyd@gmail.com>
2023-02-28 12:00:27 +0100

    Add variable

--- hello.js
+++ hello.js
@@ -1,4 +1,7 @@
-var Kristof = "is awesome";
+const Kristof = "is awesome";
+const TLOU = "is a very cool show";
 console.log("Kristof " + Kristof);
 const isKristofNice = true;
 console.log("Is Kristof nice? " + isKristofNice)
+console.log("The Last of Us " + TLOU);
+
